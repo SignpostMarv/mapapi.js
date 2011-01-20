5aab029cad546f93873a01558f3a0cce01aba5c2
SignpostMarv <github@signpostmarv.name>
2011-01-20 08:50:47 +0000

    Using @license annotation so the closure compiler can retain the license text, allowing the shell scripts to be one-liners

--- utils/closure.bat
+++ utils/closure.bat
@@ -1,4 +1,1 @@
-java -jar ..\..\compiler.jar --js ..\src\slmapapi.js --js_output_file ..\build\slmapapi-min.js
-
-copy /b ..\LICENSE + ..\build\slmapapi-min.js ..\build\slmapapi.js
-del ..\build\slmapapi-min.js
+java -jar ..\..\compiler.jar --js ..\src\slmapapi.js --js_output_file ..\build\slmapapi.js
