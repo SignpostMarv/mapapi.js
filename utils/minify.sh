48ff80939b95913bf5c657ae3b1c6e4de9c566cd
SignpostMarv <github@signpostmarv.name>
2011-04-03 11:08:20 +0100

    Decided not to include the build directory in the repository, as it leads to otherwise useless commits for now

--- utils/minify.sh
+++ utils/minify.sh
@@ -1,3 +1,5 @@
+mkdir -p ../build/
+
 java -jar ../../compiler.jar --js ../src/slmapapi.js --js_output_file ../build/slmapapi.js
 java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/slmapapi.css ../src/slmapapi.css
 
