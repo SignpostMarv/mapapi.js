573faac0d7a86ef1fccb857e9d61662649e20f13
SignpostMarv <github@signpostmarv.name>
2012-01-12 20:36:29 +0000

    since the bash script can be run under gitbash on windows, I am tossing out the windows batch script instead of trying to port the bash script upgrades to windows batch ugliness

--- utils/minify.sh
+++ utils/minify.sh
@@ -1,3 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
 mkdir -p ../build/
 
 java -jar ../../compiler.jar --js ../src/slmapapi.js --js_output_file ../build/slmapapi.js
@@ -5,3 +7,6 @@ java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/s
 
 java -jar ../../compiler.jar --js ../src/mapapi.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/google-v3.js --js ../src/renderer/canvas.js --js_output_file ../build/mapapi.js
 java -jar ../../compiler.jar --js ../src/gridconfig/com.secondlife.agni.js --js_output_file ../build/mapapi.gridConfigs.js
+
+java -jar ../../compiler.jar --js ../src/EventTarget.js --js ../src/mapapi.js --js ../src/mapapi.shape.js --js ../src/mapapi.ui.js --js ../src/ui/minimalist.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/canvas.js --js ../src/renderer/google-v3.js --js_output_file ../build/mapapi-complete.js
+gzip -cf --best ../build/mapapi-complete.js > ../build/mapapi-complete.js.gz
