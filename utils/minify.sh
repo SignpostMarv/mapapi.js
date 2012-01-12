dc4bb2d597d56cbdffb9cb1115ab468191f54752
SignpostMarv <github@signpostmarv.name>
2012-01-12 22:52:55 +0000

    adding a method to generate configs for Aurora-based grids

--- utils/minify.sh
+++ utils/minify.sh
@@ -8,5 +8,5 @@ java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/s
 java -jar ../../compiler.jar --js ../src/mapapi.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/google-v3.js --js ../src/renderer/canvas.js --js_output_file ../build/mapapi.js
 java -jar ../../compiler.jar --js ../src/gridconfig/com.secondlife.agni.js --js_output_file ../build/mapapi.gridConfigs.js
 
-java -jar ../../compiler.jar --js ../src/EventTarget.js --js ../src/mapapi.js --js ../src/mapapi.shape.js --js ../src/mapapi.ui.js --js ../src/ui/minimalist.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/canvas.js --js ../src/renderer/google-v3.js --js_output_file ../build/mapapi-complete.js
+java -jar ../../compiler.jar --js ../src/EventTarget.js --js ../src/mapapi.js --js ../src/mapapi.shape.js --js ../src/mapapi.ui.js --js ../src/ui/minimalist.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/canvas.js --js ../src/renderer/google-v3.js --js ../src/gridconfig/aurora-sim.js --js_output_file ../build/mapapi-complete.js
 gzip -cf --best ../build/mapapi-complete.js > ../build/mapapi-complete.js.gz
