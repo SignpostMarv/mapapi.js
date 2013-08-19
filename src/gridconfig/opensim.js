6a727eee697ccca8c4477dc97934ca1caf9804b2
SignpostMarv <github@signpostmarv.name>
2013-08-20 00:04:45 +0100

    opensim map tiles now load on canvas renderer but cause a 'Uncaught RangeError: Maximum call stack size exceeded' error message relating to zoomRange_changed in the google renderer. mapapi.net implementation also needs tweaking to make sure history state tweaks use correct url (they currently use a secondlife prefix which break page reloads).

--- src/gridconfig/opensim.js
+++ src/gridconfig/opensim.js
@@ -9,10 +9,10 @@
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,117 +37,161 @@
 	}
 	var
 		gridPoint   = mapapi['gridPoint'],
+		tileSource  = mapapi['tileSource'],
 		gridConfig  = mapapi['gridConfig'],
 		gridConfigs = (mapapi['gridConfigs'] = mapapi['gridConfigs'] || {})
 	;
-	
-	gridConfigs['opensim'] = function(uri){
+
+	gridConfigs['opensim'] = function(uri, success, fail){
 		var
-			req = new xhr
-		;
-		req['onload'] = function(e){
-			var
-				resp
-			;
-			resp = JSON['parse'](e['target']['responseText']);
-			if(resp && resp['hasOwnProperty']('config')){
-				resp = resp['config'];
-				['HTTP', 'WebSocket', 'MapImageURI'].forEach(function(e){
-					if(!resp['hasOwnProperty'](e)){
-						throw 'Config missing required property (' + e + ')';
-					}
-				});
-				var
-					opensim = new gridConfig({
-						'pos2region' : function(pos, success, fail){
-							var
-								pos    = gridPoint['fuzzy'](pos);
-								x      = Math.floor(pos['x']),
-								y      = Math.floor(pos['y']),
-								apireq = new xhr
-							;
-							apireq['onload'] = function(e){
-								var
-									resp      = JSON['parse'](e.target.responseText),
-									isSuccess = true
-								;
-								if(resp['hasOwnProperty']('error')){
-									fail(resp['error']);
-								}else{
-									['x','y','region'].forEach(function(e){
-										if(!resp['hasOwnProperty'](e)){
-											isSuccess = false;
+			fail    = fail || function(e){
+				throw e;
+			},
+			req     = undefined,
+			origUri = uri,
+			p9000   = ':9000',
+			p8002   = ':8002',
+			testing = 0,
+			open    = function(){
+				req = new xhr;
+				req['onload'] = function(e){
+					var
+						resp
+					;
+					resp = JSON['parse'](e['target']['responseText']);
+					if(resp && resp['hasOwnProperty']('config')){
+						resp = resp['config'];
+						['HTTP', 'WebSocket', 'MapImageURI'].forEach(function(e){
+							if(!resp['hasOwnProperty'](e)){
+								fail('Config missing required property (' + e + ')');
+							}
+						});
+						var
+							ts = new tileSource({
+								'copyright' : 'foo',
+								'label'     : 'Land & Objects'
+							}),
+							opensim = new gridConfig({
+								'tileSources' : [
+									ts
+								],
+								'pos2region' : function(pos, success, fail){
+									var
+										pos    = gridPoint['fuzzy'](pos);
+										x      = Math.floor(pos['x']),
+										y      = Math.floor(pos['y']),
+										apireq = new xhr
+									;
+									apireq['onload'] = function(e){
+										var
+											resp      = JSON['parse'](e.target.responseText),
+											isSuccess = true
+										;
+										if(resp['hasOwnProperty']('error')){
+											fail(resp['error']);
+										}else{
+											['x','y','region'].forEach(function(e){
+												if(!resp['hasOwnProperty'](e)){
+													isSuccess = false;
+												}
+											});
+											if(!isSuccess){
+												fail('API call was missing required properties');
+											}else{
+												success({
+													'pos'    : gridPoint['fuzzy'](resp),
+													'region' : resp['region']
+												});
+											}
 										}
-									});
-									if(!isSuccess){
-										fail('API call was missing required properties');
-									}else{
-										success({
-											'pos'    : gridPoint['fuzzy'](resp),
-											'region' : resp['region']
-										});
-									}
-								}
-							};
-							apireq['onerror'] = function(e){
-								fail(e);
-							};
-							apireq['open']('GET', uri + '/pos2region/' + escape(x) + '/' + escape(y), true);
-							apireq['send']();
-						},
-						'region2pos' : function(region, success, fail){
-							var
-								apireq = new xhr
-							;
-							apireq['onload']  = function(e){
-								var
-									resp      = JSON['parse'](e.target.responseText),
-									isSuccess = true
-								;
-								if(resp['hasOwnProperty']('error')){
-									fail(resp['error']);
-								}else{
-									['x','y','region'].forEach(function(e){
-										if(!resp['hasOwnProperty'](e)){
-											isSuccess = false;
+									};
+									apireq['onerror'] = function(e){
+										fail(e);
+									};
+									apireq['open']('GET', uri + '/pos2region/' + escape(x) + '/' + escape(y), true);
+									apireq['send']();
+								},
+								'region2pos' : function(region, success, fail){
+									var
+										apireq = new xhr
+									;
+									apireq['onload']  = function(e){
+										var
+											resp      = JSON['parse'](e.target.responseText),
+											isSuccess = true
+										;
+										if(resp['hasOwnProperty']('error')){
+											fail(resp['error']);
+										}else{
+											['x','y','region'].forEach(function(e){
+												if(!resp['hasOwnProperty'](e)){
+													isSuccess = false;
+												}
+											});
+											if(!isSuccess){
+												fail('API call was missing required properties');
+											}else{
+												success({
+													'pos'    : gridPoint['fuzzy'](resp),
+													'region' : resp['region']
+												});
+											}
 										}
-									});
-									if(!isSuccess){
-										fail('API call was missing required properties');
-									}else{
-										success({
-											'pos'    : gridPoint['fuzzy'](resp),
-											'region' : resp['region']
-										});
-									}
+									};
+									apireq['onerror'] = function(e){
+										fail(e);
+									};
+									apireq['open']('GET', uri + '/region2pos/' + escape(region), true);
+									apireq['send']();
 								}
-							};
-							apireq['onerror'] = function(e){
-								fail(e);
-							};
-							apireq['open']('GET', uri + '/region2pos/' + escape(region), true);
-							apireq['send']();
+							})
+						;
+						ts['getTileURL'] = function(pos, zoom){
+							var
+								pos                   = gridPoint['fuzzy'](pos),
+								sl_zoom               = Math.floor(zoom + 1),
+								regions_per_tile_edge = Math.pow(2, sl_zoom - 1),
+								x                     = pos['x'],
+								y                     = pos['y']
+							;
+							x -= x % regions_per_tile_edge;
+							y -= y % regions_per_tile_edge;
+
+							if(x < 0 || y < 0){
+								return null;
+							}
+							return [resp['MapImageURI'] + '/map', sl_zoom, x, y, "objects.jpg"].join("-");
+						};
+						if(success){
+							success(opensim);
 						}
-					})
-				;
-				opensim.pos2region({'x':1000, 'y':1000}, function(e){
-					console.log(['pos2region success', e]);
-				}, function(e){
-					console.log(['pos2region fail', e]);
-				});
-				opensim.region2pos('test', function(e){
-					console.log(['pos2region success', e]);
-				}, function(e){
-					console.log(['pos2region fail', e]);
-				});
-			}else{
-				throw 'HTTP access to URI successful, no config object found (' + uri + ')';
+					}else{
+						fail('HTTP access to URI successful, no config object found (' + uri + ')');
+					}
+				};
+				req['onerror'] = function(e){
+					if(!/([^\:])+\:\d+/.test(origUri)){
+						if(uri == origUri){
+							testing = 1;
+							uri = uri['replace'](/^(https?\:\/\/)([^\/]+)(.*)/,function(m,m1,m2,m3){
+								return m1 + m2 + p9000 + m3;
+							});
+							open();
+						}else if(testing == 1){
+							testing = 2;
+							uri = uri['replace'](/^(https?\:\/\/)([^\/]+)(.*)/,function(m,m1,m2,m3){
+								return m1 + m2 + 8002 + m3;
+							});
+							open();
+						}else{
+							fail(e);
+						}
+					}
+				}
+				req['open']('GET', uri, true);
+				req['send']();
 			}
-		};
-		req['onerror'] = function(e){
-			throw e;
-		}
-		req['open']('GET', uri, true);
-		req['send']();
+		;
+		open();
 	};
 })();
