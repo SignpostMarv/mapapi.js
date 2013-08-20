695e78913fa8b071302a713e6fd6df0a87299b6b
SignpostMarv <github@signpostmarv.name>
2013-08-21 00:13:52 +0100

    multi-lining the minification script to make branch merging easier

--- utils/minify.sh
+++ utils/minify.sh
@@ -2,13 +2,37 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 mkdir -p ../build/
 
-java -jar ../../compiler.jar --js ../src/slmapapi.js --js_output_file ../build/slmapapi.js
+java -jar ../../compiler.jar \
+	--js ../src/slmapapi.js \
+	--js_output_file ../build/slmapapi.js
 java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/slmapapi.css ../src/slmapapi.css
 
-java -jar ../../compiler.jar --js ../src/mapapi.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/google-v3.js --js ../src/renderer/canvas.js --js_output_file ../build/mapapi.js
-java -jar ../../compiler.jar --js ../src/gridconfig/com.secondlife.agni.js --js_output_file ../build/mapapi.gridConfigs.js
+java -jar ../../compiler.jar \
+	--js ../src/mapapi.js \
+	--js ../src/mapapi.renderer.js \
+	--js ../src/mapapi.tileSource.js \
+	--js ../src/mapapi.gridConfig.js \
+	--js ../src/renderer/google-v3.js \
+	--js ../src/renderer/canvas.js \
+	--js_output_file ../build/mapapi.js
+java -jar ../../compiler.jar \
+	--js ../src/gridconfig/com.secondlife.agni.js \
+	--js_output_file ../build/mapapi.gridConfigs.js
 
-java -jar ../../compiler.jar --js ../src/EventTarget.js --js ../src/mapapi.js --js ../src/mapapi.shape.js --js ../src/mapapi.ui.js --js ../src/ui/minimalist.js --js ../src/mapapi.renderer.js --js ../src/mapapi.tileSource.js --js ../src/mapapi.gridConfig.js --js ../src/renderer/canvas.js --js ../src/renderer/google-v3.js --js ../src/gridconfig/aurora-sim.js --js_output_file ../build/mapapi-complete.js
+java -jar ../../compiler.jar \
+	--js ../src/EventTarget.js \
+	--js ../src/mapapi.js \
+	--js ../src/mapapi.shape.js \
+	--js ../src/mapapi.ui.js \
+	--js ../src/ui/minimalist.js \
+	--js ../src/mapapi.renderer.js \
+	--js ../src/mapapi.tileSource.js \
+	--js ../src/mapapi.gridConfig.js \
+	--js ../src/renderer/canvas.js \
+	--js ../src/renderer/google-v3.js \
+	--js ../src/gridconfig/aurora-sim.js \
+	--js_output_file ../build/mapapi-complete.js
+
 if [ -f ../../7za.exe ];
 then
 	if [ -f ../build/mapapi-complete.js.gz ];
