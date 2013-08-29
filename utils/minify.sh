a1137bf8ee111d231db87749560008b0c5a68a8b
SignpostMarv <github@signpostmarv.name>
2013-08-29 17:27:43 +0100

    ommitting reduced mapapi.js from build process

--- utils/minify.sh
+++ utils/minify.sh
@@ -9,14 +9,6 @@ java -jar ../../compiler.jar \
 java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/slmapapi.css ../src/slmapapi.css
 
 java -jar ../../compiler.jar \
-	--js ../src/mapapi.js \
-	--js ../src/mapapi.renderer.js \
-	--js ../src/mapapi.tileSource.js \
-	--js ../src/mapapi.gridConfig.js \
-	--js ../src/renderer/google-v3.js \
-	--js ../src/renderer/canvas.js \
-	--js_output_file ../build/mapapi.js
-java -jar ../../compiler.jar \
 	--js ../src/gridconfig/com.secondlife.agni.js \
 	--js_output_file ../build/mapapi.gridConfigs.js
 
