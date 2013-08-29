8029b0262dc9753a5de5f2736ecb06048efce83e
SignpostMarv <github@signpostmarv.name>
2013-08-29 17:23:36 +0100

    minor layout tweak

--- utils/minify.sh
+++ utils/minify.sh
@@ -5,6 +5,7 @@ mkdir -p ../build/
 java -jar ../../compiler.jar \
 	--js ../src/slmapapi.js \
 	--js_output_file ../build/slmapapi.js
+
 java -jar ../../yuicompressor-2.4.2.jar --type css --charset utf-8 -o ../build/slmapapi.css ../src/slmapapi.css
 
 java -jar ../../compiler.jar \
