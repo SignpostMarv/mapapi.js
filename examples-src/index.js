3947e91498f9d931ac5b0d23117976300341b094
SignpostMarv <me@signpostmarv.name>
2018-02-18 12:30:07 +0000

    experimenting with effect of preload on performance

---
+++ examples-src/index.js
@@ -0,0 +1,49 @@
+import {Agni} from '../src/Grids/Agni.js';
+import {Canvas2dTileRenderer} from '../src/Renderer.js';
+import {BasicUserInterface} from '../src/UserInterface.js';
+
+export function init() {
+
+
+    const AgniInstance = new Agni();
+
+    const renderer = new Canvas2dTileRenderer(1, 1, AgniInstance.tileSources[0], 0, [1000, 1000]);
+
+    AgniInstance.tileSources[0].fireTileupdateOnImgTile = false;
+
+    const UI = new BasicUserInterface(renderer);
+    const move = (e) => {
+        e.target.renderer.animator.animate(e.detail.position);
+    };
+    UI.addEventListener('click', move);
+    UI.addEventListener('dragmove', move);
+    UI.addEventListener('wheel', (e) => {
+        const renderer = e.target.renderer;
+        renderer.animator.animate(
+            UI.mousePosition,
+            renderer.zoom + ((e.detail.zoomIn ? -.5 : +.5) * (e.detail.amount / 100))
+        );
+    });
+    UI.draggable = true;
+    UI.wheelZoom = true;
+
+    document.body.appendChild(renderer.DOMNode);
+
+    const sync = () => {
+        if (renderer.dirty) {
+            renderer.render();
+        }
+
+        requestAnimationFrame(sync);
+    };
+
+    requestAnimationFrame(() => {
+        renderer.updateAsClientSize();
+
+        sync();
+    });
+
+    window.Agni = AgniInstance;
+    window.renderer = renderer;
+    window.ui = UI;
+}
